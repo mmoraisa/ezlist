2bc847c50db6cebcb5fe306adfadaef8d100e9af
Murillo de Morais <mm@in3d.com.br>
2018-06-01 22:06:58 -0300

    feat: added customized status bar

--- application/screens/Main.js
+++ application/screens/Main.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import EzlistStatusBar from '../components/general/EzlistStatusBar'
+import { black } from '../helpers/Colors'
+
 const Main = () => {
     return (
-        <View>
+        <View style={{ flex: 1 }}>
+            <EzlistStatusBar
+                backgroundColor={ black }
+                barStyle='light-content'
+            />
             <Text>Main</Text>
         </View>
     )
